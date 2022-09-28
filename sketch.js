28d975130dc05941e93c8918e189b5a4d32806d3
zaldeth <113926156+zaldeth@users.noreply.github.com>
2022-09-28 21:15:06 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,114 @@
-var path,boy, leftBoundary,rightBoundary;
-var pathImg,boyImg;
-var i;
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+
 
 function preload(){
-  pathImg = loadImage("path.png");
-  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
 }
+
 
 function setup(){
   
   createCanvas(400,400);
-  
 // Moving background
-path=createSprite(200,200);
-path.addImage(pathImg);
-path.velocityY = 4;
-path.scale=1.2;
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
 
 //creating boy running
-boy = createSprite(180,340,30,30);
-boy.scale=0.08;
-boy.addAnimation("JakeRunning",boyImg);
-  
-
-leftBoundary=createSprite(0,0,100,800);
-
-//leftBoundary.invisible = false;
- //leftBoundary.visible = true;
-// leftBoundary.invisible = true;
-  leftBoundary.visible = false;
-
-
-rightBoundary=createSprite(410,0,100,800);
-rightBoundary.visible = false;
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
 }
 
 function draw() {
   background(0);
-  path.velocityY = 4;
   
-  boy.x = World.mouseX;
+  // boy moving on Xaxis with mouse'
+  rabbit.x = World.mouseX;
   
   edges= createEdgeSprites();
-  boy.collide(edges[3]);
-  boy.collide(leftBoundary);
-  boy.collide(rightBoundary);
+  rabbit.collide(edges);
   
-  //code to reset the background
-
-  /*if(path.y > 400 ){
-    path.y = height/2;
-  }*/
-
-  /*if(path.y > 400 ){
+   drawSprites();
    
-  path.y = height/2;
-  }*/
-
-  /*if(path.y > 400 ){
-path.y = height/2;}*/
-
-if(path.y > 400 ){path.y = height/2};
   
-  drawSprites();
+// var select_sprites = Math(random(1,3));
+
+// var select_sprites = Math.random(random(1,3));
+
+// var select_sprites = Math.round(1,3);
+
+var select_sprites = Math.round(random(1,3));
+
+  
+  // if (frameCount % 50 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+   if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }
+   }
+
+  // if (frameCount / 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+  // if (frameCount % 80 = 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+
+
+}
+
+function createApples() {
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+  
+}
+
+function createOrange() {
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+}
+
+function createRed() {
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
 }
